4fa58b56580cb7e600bb573819484737a3404092
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2023-10-13 21:30:18 -0700

    Finish 02
    Change all tests to not skip. Create function which repeats certain string (str) a certain number (num) of times. Return 'ERROR' if negative num

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str = 'str', num = 1) {
+    let newStr = '';
 
+    if (num < 0) {
+        return 'ERROR';
+    } 
+
+    while (num) {
+        newStr += str;
+        num--;
+    }
+    
+    return newStr;
 };
 
 // Do not edit below this line
