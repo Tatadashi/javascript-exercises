f17e8c2a762ce8271d35776591600f637e56dec7
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2023-10-17 04:46:52 -0700

    Finish 05
    Pass all tests. Use series of if else to determine if parameters are non-negative integers, then use for loop to add them, returning sum. Return 'ERROR' if fail any if else

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1 = 1, num2 = 3) {
+    if (Number.isInteger(num1) && Number.isInteger(num2)) {
+        let startNum = num1;
+        let endNum = num2;
 
+        if (num1 > num2) {
+            startNum = num2;
+            endNum = num1;
+        }
+
+        if (startNum >= 0) {
+            let sum = 0;
+
+            for (let i = startNum; i <= endNum; i++) {
+                sum += i;
+            }
+
+            return sum;
+        }
+    }
+    return 'ERROR';
 };
 
 // Do not edit below this line
