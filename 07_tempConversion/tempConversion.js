0422c395a542b27c7f635818f4d20cbfa2b81aee
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2023-10-17 05:14:19 -0700

    Finish 07
    Pass all tests. Convert fahrenheit to celsius and vice versa. Round number to 1 decimal with no fixed point (i.e. no 0.0)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitNum = 32) {
+  let celsiusNum = (fahrenheitNum - 32) * (5 / 9);
+  celsiusNum = Math.round(celsiusNum * 10) / 10;
+
+  return celsiusNum;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusNum = 0) {
+  let fahrenheitNum = (celsiusNum * (9 / 5)) + 32;
+  fahrenheitNum = Math.round(fahrenheitNum * 10) / 10;
+
+  return fahrenheitNum;
 };
 
 // Do not edit below this line
