af22618e19b2f76d30e36547bf2c6c0bdbd7702d
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2023-11-20 00:56:43 -0800

    Finish 08
    Create functions for addition, subtraction, sum of array, product of array, power, and factorial

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  for (let num of array) {
+    sum += num;
+  }
+	return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1;
+  for (let num of array) {
+    product *= num;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	let solution = 1;
+  for (let i = 0; i < power; i++) {
+    solution *= base;
+  }
+  return solution;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let solution = 1;
+  for (let i = 1; i <= number; i++) {
+    solution *= i;
+  }
+  return solution;
 };
 
 // Do not edit below this line
