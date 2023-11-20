0a850b2b32cab8e87c55485dd90863b303a23ae8
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2023-11-20 01:48:24 -0800

    Finish 09
    Use regex, string function, and array functions to check if string is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.replaceAll(/[^a-zA-Z0-9]/g, ``);
+    string = string.toLowerCase();
 
+    let reversed = string.split(``);
+    reversed = reversed.reverse();
+    reversed = reversed.join(``);
+
+    return string == reversed;
 };
 
 // Do not edit below this line
