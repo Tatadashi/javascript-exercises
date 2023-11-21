eeb24af24d120a350b34af6e738cbe70bea2c98f
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2023-11-21 03:40:23 -0800

    Finish 10
    Use for loop with conditionals for solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 1) {
+        return `OOPS`;
+    }
 
+    let currentMember = 1;
+    let lastMember = 1;
+    
+    if (num < 2) {
+        return 1;
+    }
+
+    for (let i = 2; i < num; i++) {
+        let tempNumber = currentMember;
+        currentMember += lastMember;
+        lastMember = tempNumber;
+    }
+
+    return currentMember;
 };
 
 // Do not edit below this line
